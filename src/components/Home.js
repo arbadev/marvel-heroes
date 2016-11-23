c37b4544b5c6e3c07c8685c5e4cc65ad91cebd8d
Andres Barradas <arbadev@gmail.com>
2016-11-23 15:05:34 -0400

    fix node-modules issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-
 import SearchBar from './SearchBar'
 import HeroeCard from './HeroeCard'
 import Board from './Board'
+import ReactPaginate from 'react-paginate';
+import '../styles/Home.scss'
 
 
 class Home extends Component {
@@ -11,6 +12,16 @@ class Home extends Component {
       <div>
         <SearchBar/>
         <Board/>
+          <ReactPaginate previousLabel={"previous"}
+                         nextLabel={"next"}
+                         breakLabel={<a href="">...</a>}
+                         breakClassName={"break-me"}
+                         marginPagesDisplayed={2}
+                         pageRangeDisplayed={5}
+                         clickCallback={this.handlePageClick}
+                         containerClassName={"pagination"}
+                         subContainerClassName={"pages pagination"}
+                         activeClassName={"active"} />
       </div>
     )
   }
