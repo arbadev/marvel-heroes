33f099631fb27fe9bad3ff6e05f8be8a031f06ba
Andres Barradas <arbadev@gmail.com>
2016-11-26 19:40:38 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict'
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const publicFiles = path.join(__dirname, '/dist')
+app.use(express.static(publicFiles))
+// app.use(express.static('index_bundle.js'))
+app.get('/', (req, res) => {
+  const index = path.join(__dirname, '/dist/index.html')
+  res.sendFile(index)
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log('server runing')
+})
